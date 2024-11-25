16c8da8b205702514ab206166d8079c46c9f8c90
Yakovemny <yakovemn@gmail.com>
2024-11-25 14:36:39 +0200

    uploaded to github

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+    public static int maxNumbers(int a,int b, int c){{
+        int ans = Math.max(a,b);
+        return Math.max(ans , c);
+    }
+
+    }
+}
